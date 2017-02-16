111b03c296083bb5824431ac31b4f55110936787
sanyo112 <smbhat93@gmail.com>
2017-02-16 12:28:10 -0500

    Initial_value changed to price

--- src/databean/FundBean.java
+++ src/databean/FundBean.java
@@ -8,22 +8,22 @@ public class FundBean {
 	private int fundId;
 	private String name;
 	private String symbol;
-	private String initial_value;
+	private String price;
 	
 	public FundBean() {}
-	public FundBean(String name, String symbol, String initial_value) {
+	public FundBean(String name, String symbol, String price) {
 		this.name = name;
 		this.symbol = symbol;
-		this.initial_value = initial_value;
+		this.price = price;
 	}
 	public String getName() 		{return name;}
 	public String getSymbol() 		{return symbol;}
-	public String getInitial_value() 	{return initial_value;}
+	public String getPrice() 	{return price;}
 	public int getFundId() 			{return fundId;}
 	
 	public void setName(String name) 					{this.name = name;}
 	public void setSymbol(String symbol) 				{this.symbol = symbol;}
-	public void setInitial_value(String initial_value) 	{this.initial_value =  initial_value;}
+	public void setPrice(String price) 					{this.price =  price;}
 	public void setFundId(int fundId) 					{this.fundId = fundId;}
 	
 	
